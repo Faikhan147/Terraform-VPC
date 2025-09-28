ecd3bb2eabd45c6afaeca54e8430f33acd925242
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-28 15:03:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-backend-all-envs"
-    key            = "eks/values/prod/terraform.tfstate"
+    key            = "vpc/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-locks-vpc"
