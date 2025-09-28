6af82891cc7e604d8493d7fdc451bf1577a7381a
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-28 14:42:49 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "env" {
+type = string
+}
+
+
+variable "region" {
+type = string
+}
+
+
+variable "vpc_cidr" {
+type = string
+}
+
+
+variable "azs" {
+description = "List of AZs to use (order matters)"
+type = list(string)
+}
+
+
+variable "public_subnets" {
+description = "List of CIDRs for public subnets (same length as azs)"
+type = list(string)
+}
+
+
+variable "private_subnets" {
+description = "List of CIDRs for private subnets (same length as azs)"
+type = list(string)
+}
+
+
+variable "enable_flow_logs" {
+type = bool
+}
+
+
+variable "tags" {
+type = map(string)
+}
